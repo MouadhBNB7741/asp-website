c518f93bc3d717cdf74d31cbbc4f7a7bd2293ef4
MouadhBNB7741 <129070196+MouadhBNB7741@users.noreply.github.com>
2024-06-25 23:24:55 +0100

    Add files via upload

---
+++ zello.js
@@ -0,0 +1,70 @@
+/*
+so here imma disguest every advanced concept
+abstract tfakar the philosophie concept of triangle ta3 not same drawing but can be reconized
+base is used to use element from overrided methods
+3andak linq in the system is used to add more methods to objects for example imagine you have a class that have an 
+double attribute to do it sum just type .sum(e => e.thatattri;)
+can use the where key word too to specifie where you want the change
+key word where used to specifie things ex you want to specifie a generic beli howa class T:class
+we the async await in here +matansach to use generics
+generics really importants un c# 5atach t5alih yghati 3la weaknesses ta3ah bah ywalo fel level ta3 py and js
+
+delegates
+delegates 3tiha a scope bah ta5dam fih
+basic idea howa to pass a method by reference nrmlment used as anonymous function
+u generaler dont have to dirha out side arrow func chaya just for help tfakar beli they have same return params types
+delegate type name(params);
+
+bitch i have pointer pointer chi cheeky one after learning they all do have it so i was wrong thx
+
+so delegates can be conbined by using the + operator and you can pass delegates as arguments
+*/
+//https://www.youtube.com/watch?v=0QUgvfuKvWU
+//kamal mena bcz la5or use so much weird staff
+// const a = 5; //101
+// const b = 3; //011
+// console.log(a ^ b); //110=6
+
+/*asp  stand proud you r strong but django still better
+best part is it is just like node or django the mvc
+microsoft da3matah bah ywali yasna3lak auto staff like superuser staff in django
+
+controller is the controller folder it return views base on functions names
+you can create as much controller as you want it is the same as node or django apps but they r simpler
+
+Models is the Models folder
+there is wa7da wajda but you should add yours 
+same as django create a class as a database chaya it need constructor
+
+now where the asp is strong is that it auto generates code it is the same as .net 
+it make a faster building but still django better with restAPI
+
+migration they are accesed from from data folder same as django chaya it sucks bcz you have to runapp to do it
+do the changes in package manager console tfakar each database 
+sna3alha table generate auto crud and then 
+dir 
+enable-migrations
+add-migration name
+ki dirha di tal9a database history as django 
+
+use update-database command
+this will create tables for your database
+
+to see your database 7awas 3la sql server object explorer
+it is a bunch of shit chaya chaba it display your database in a nice way
+and can be modified
+the best part about asp is that those right click you do really create both back and front
+it is as django chaya better chwiya but still django api better
+
+
+now why node is better node can do the same faster but fel heavy staff lo5rin 5ir(faster) 
+node is easier to spot problems bcz you created everything meanwhile asp you do right click django chwiya dangerous
+node can be used by any pattern mvc with orm object rational(ta3 each table have a class) mapping or old fashion way dio
+*/
+
+class App {
+  static Main() {
+    console.log("hello world");
+  }
+}
+App.Main();
