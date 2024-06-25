d2472b0fbdd69d598b38d8a816ff9aa9271f1f2e
MouadhBNB7741 <129070196+MouadhBNB7741@users.noreply.github.com>
2024-06-25 23:19:16 +0100

    Add files via upload

---
+++ Models/Joke.cs
@@ -0,0 +1,13 @@
+namespace firstApp.Models
+{
+    public class Joke
+    {
+        public int Id { get; set; }
+        public string jokeQuestion { get; set; }
+        public string jokeAnswer { get; set; }
+        public Joke()
+        {
+
+        }
+    }
+}
